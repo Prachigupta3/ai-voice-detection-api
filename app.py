4af5feddffb9574e2f33b6e772438063f76cb4c5
Prachigupta3 <prachi.gupta.prachi.1961@gmail.com>
2026-02-04 01:36:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,5 +95,6 @@ def voice_detection():
         "confidenceScore": round(confidence, 2),
         "explanation": explanation
     })
-    if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+ if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
